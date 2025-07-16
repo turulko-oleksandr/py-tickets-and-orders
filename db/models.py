cb8f66dfe4210bac189a41aba6822eca4d0b2fab
turulko-oleksandr <SashaTurulko@i.ua>
2025-07-16 14:32:14 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -98,9 +98,11 @@ class Ticket(models.Model):
 
     def __str__(self) -> str:
         return (
-            f"{self.movie_session.movie.title} "
-            f"{self.movie_session.show_time.strftime("%Y-%m-%d %H:%M:%S")} "
-            f"(row: {self.row}, seat: {self.seat})"
+            self.movie_session.movie.title + " " +
+            self.movie_session
+            .show_time
+            .strftime("%Y-%m-%d %H:%M:%S") +
+            f" (row: {self.row}, seat: {self.seat})"
         )
 
     def clean(self) -> None:
